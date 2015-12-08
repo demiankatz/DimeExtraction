181c844fd9bcb4e9c49a30b8f2cceb6fadb4b8b7
Demian Katz <demian.katz@villanova.edu>
2015-12-08 15:37:08 -0500

    Script fixes.

--- generate_everything.sh
+++ generate_everything.sh
@@ -16,12 +16,12 @@ for file in alchemyapi-out/entities*.txt; do
 done
 
 echo Creating CSV summaries of Spotlight entities...
-for file in spotlight-out/entities*.json; do
+for file in spotlight-out/*.json; do
     php SpotlightEntitiesToCsv.php $file
 done
 for file in raw-texts/*; do
     base=`basename $file`
-    cat spotlight-out/entities*${base/.txt/.csv} > spotlight-out/combined-entities-${base/.txt/.csv}
+    cat spotlight-out/*${base/.txt/.csv} > spotlight-out/combined-entities-${base/.txt/.csv}
 done
 for file in spotlight-out/combined-entities*; do
     php SpotlightEntitySummarizer.php $file
