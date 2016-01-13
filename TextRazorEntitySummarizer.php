702f4dfb6479ce5ed9445708044c827f99693c47
Demian Katz <demian.katz@villanova.edu>
2016-01-13 07:40:48 -0500

    Merge variant case forms.

--- TextRazorEntitySummarizer.php
+++ TextRazorEntitySummarizer.php
@@ -4,10 +4,11 @@ $handle = fopen($argv[1], 'r');
 
 $header = fgetcsv($handle);
 $header[] = 'Count';
+$header[] = 'Variant Forms';
 
 $data = [];
 while ($line = fgetcsv($handle)) {
-    $id = $line[0];
+    $id = strtolower($line[0]);
 	if (!isset($data[$id])) {
 	    $data[$id] = [$line];
 	} else {
@@ -21,7 +22,7 @@ fputcsv($handle, $header);
 
 foreach ($data as $group) {
 	$count = $relevance = $confidence = 0;
-	$lastLine = $matchedTexts = [];
+	$forms = $lastLine = $matchedTexts = [];
 	foreach ($group as $line) {
 	    if ($count > 0) {
 		    for ($i = 2; $i < 4; $i++) {
@@ -33,16 +34,40 @@ foreach ($data as $group) {
 			}
 		}
 		$count++;
+        $forms[] = $line[0];
 		$matchedTexts[] = $line[1];
 		$confidence += $line[4];
 		$relevance += $line[5];
 		$lastLine = $line;
 	}
+    $line[0] = pickBestForm($forms);
 	$line[1] = implode(', ', array_unique($matchedTexts));
 	$line[4] = $confidence / $count;
 	$line[5] = $relevance / $count;
 	$line[] = $count;
+    $line[] = implode(', ', array_unique($forms));
 	fputcsv($handle, $line);
 }
 
 fclose($handle);
+
+function pickBestForm($forms)
+{
+    $upper = $lower = $mixed = false;
+    foreach ($forms as $form) {
+        if (strtoupper($form) === $form) {
+            $upper = $form;
+        } else if (strtolower($form) === $form) {
+            $lower = $form;
+        } else {
+            // mixed is most preferred
+            return $form;
+        }
+    }
+    if ($lower) {
+        // lower is second best
+        return $lower;
+    }
+    // upper is last resort
+    return $upper;
+}
